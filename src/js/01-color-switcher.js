989999e3170c328453972610c20f1706e4a48f0d
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-03-01 19:26:55 +0200

    task 1 start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,29 @@
 
-function getRandomHexColor() {
+ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+// console.log(startBtn,stopBtn,document)
+
+
+
+
+const start = () => {
+  timerId = setInterval(() => {
+    // const createRandomColor = getRandomHexColor();
+    body.style.backgroundColor = getRandomHexColor();
+    
+  }, 1000)
+};
+const stop = () => { 
+  clearInterval(timerId)
+};
+
+startBtn.addEventListener('click', start);
+stopBtn.addEventListener('click', stop);

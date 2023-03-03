1b7d1419997e306cc95eec3b9c4c6360abe37cb7
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-03-03 13:26:53 +0200

    task to ger references

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,8 +13,8 @@ const options = {
     //   Створення змінних для відображення поточного часу і часу вибраного користувачем в мілісекундах
       
       
-      const pickedData = selectedDates[0].getTime();
-      const currentData = Date.now();
+    const pickedData = selectedDates[0].getTime();
+      // const currentData = Date.now();
 
 
     //   Перевірка чи вибрана дата в майбутньому
@@ -27,12 +27,60 @@ const options = {
   },
 };
 
+let currentData = Date.now();
+
 flatpickr("#datetime-picker", options);
+const fp = document.querySelector("#datetime-picker")._flatpickr;
+
+
+const refs = {
+  day: document.querySelector('span[data-days]'),
+  hour: document.querySelector('span[data-hours]'),
+  minute: document.querySelector('span[data-minutes]'),
+  second: document.querySelector('span[data-seconds]'),
+  
+}
+// console.log(refs.taymerHour)
+
+
+
+
 
 const startBtn = document.querySelector('button[data-start]');
 startBtn.disabled = true;
 // console.log(pickedData,currentData)
 
+startBtn.addEventListener('click', onTimerClick)
 
+
+function onTimerClick() {
+  const futureData = fp.selectedDates[0].getTime();
+   const ms = futureData - currentData
+   setTimeout(() => {
+console.log(ms);
+   },1000)
+  // console.log(futureData)
+ }
+
+
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
 
 

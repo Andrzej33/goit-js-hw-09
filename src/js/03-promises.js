f3537d7b69f03ae9fbb5c2bbebe89cfba261a3ce
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-03-04 00:10:14 +0200

    promises start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,28 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve("Success! Value passed to resolve function");
+        // Fulfill
+      } else {
+        reject("Error! Error passed to reject function");
+        // Reject
+      }
+      // return 5
+    }, delay);
+  })
 }
+
+
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', createPromise)
+
+
+const delayDenotation = form.delay;
+const stepDenotation = form.step;
+const amountDenotation = form.amount;
+// const delayDenotation = form.step;
+// console.log(form)

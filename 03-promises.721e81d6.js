c99aee24e564d0b2be8e57bc1e0c0cb418086bb4
Andrzej33 <Andrijvasilkiv7@gmail.com>
2023-03-04 12:33:04 +0000

    Deploying to gh-pages from @ Andrzej33/goit-js-hw-09@50ab21c8fe4052ef23803915d5d85857a9b0dee7 🚀

--- 03-promises.721e81d6.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");e.addEventListener("submit",(function(e,t){const o=Math.random()>.3;new Promise(((e,n)=>{setTimeout((()=>{o?e("Success! Value passed to resolve function"):n("Error! Error passed to reject function")}),t)}))}));e.delay,e.step,e.amount;
-//# sourceMappingURL=03-promises.721e81d6.js.map
--- 03-promises.dee9ddd7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form");e.addEventListener("submit",(function(e,t){var n=Math.random()>.3;new Promise((function(e,o){setTimeout((function(){n?e("Success! Value passed to resolve function"):o("Error! Error passed to reject function")}),t)}))}));e.delay,e.step,e.amount}();
-//# sourceMappingURL=03-promises.dee9ddd7.js.map

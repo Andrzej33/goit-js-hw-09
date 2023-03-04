50ab21c8fe4052ef23803915d5d85857a9b0dee7
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-03-04 14:32:18 +0200

    trying resolve task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,24 +1,31 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    
     setTimeout(() => {
       if (shouldResolve) {
-        resolve("Success! Value passed to resolve function");
+        resolve({position, delay});
         // Fulfill
       } else {
-        reject("Error! Error passed to reject function");
+        reject({position, delay});
         // Reject
       }
-      // return 5
+      //  
     }, delay);
   })
+  return promise
 }
+//  const p = createPromise(4,6000)
+// console.log(promise)
 
-
+// p.then(onSucces).catch(onError)
 
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', createPromise)
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+})
 
 
 const delayDenotation = form.delay;
@@ -26,3 +33,36 @@ const stepDenotation = form.step;
 const amountDenotation = form.amount;
 // const delayDenotation = form.step;
 // console.log(form)
+
+
+
+//  const promise = new Promise((resolve, reject) => {
+//   const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve("Success! Value passed to resolve function");
+//         // Fulfill
+//       } else {
+//         reject("Error! Error passed to reject function");
+//         // Reject
+//       }
+//       //  
+//     }, 2000);
+//  })
+  
+
+// promise.then(res => {
+//   console.log(res)
+// }, error => {
+//   console.log(error);
+// }
+// )
+
+
+// function promiseTest(delay) { 
+//   setTimeout(()=>{})
+// }
+
+// function createPromise(amount) {
+  
+// }
